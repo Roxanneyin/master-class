9a14ca3dfe78f743ffaf41991770fd08714adfab
Roxanneyin <roxanne.yin.1010@gmail.com>
2018-10-03 19:37:55 -0400

    added

--- EECS-545/homework 2/log_reg_newton.py
+++ EECS-545/homework 2/log_reg_newton.py
@@ -24,11 +24,12 @@ dJ = mat(np.zeros(d + 1))
 d2J = mat(np.zeros([d + 1,d + 1]))
 
 # Iteration with Newton's Method:
-N = 0; error0 = 2000
-while (error0/2000 > 0.025):
+N = 0; error0 = 10
+while (error0 > 0.01):
     z = 1 / (1 + np.exp(-theta*x_train))
     dJ = x_train*(z-y_train).T+2*lamda*theta.T
     d2J = x_train * mat(diag(multiply(z, (1-z)).getA()[0])) * x_train.T + 2*lamda*mat(np.eye(d+1,d+1))
+    error0 = np.sqrt(((d2J.I * dJ).T * (d2J.I * dJ))[0,0])
     theta = theta - (d2J.I * dJ).T
     y_train_result = theta * x_train
     for l in range(2000):
@@ -36,19 +37,16 @@ while (error0/2000 > 0.025):
             y_train_result[0,l] = 0
         else:
             y_train_result[0,l] = 1
-    error0 = 0
-    for m in range(2000):
-        if y_train_result[0,m] != y_train[0,m]:
-            error0 += 1
     N = N+1
 print("Iteration times: ", N)
-# log_like = 0
-# for l in range(2000):
-#     z = 1 / (1 + np.exp(-theta * x_train[:,l]))
-#     log_like += y_train[0,l]*np.log(z)+(1-y_train[0,l])*np.log(1-z)
-# J = -log_like + lamda*theta*theta.T
-# print(J)
-#Test data
+log_like = 0
+for l in range(2000):
+    z = 1 / (1 + np.exp(-theta * x_train[:,l]))
+    log_like += y_train[0,l]*np.log(z)+(1-y_train[0,l])*np.log(1-z)
+J = -log_like + lamda*theta*theta.T
+print("Value of objective function is: ", J[0,0])
+
+# Test data
 y_test_result = theta * x_test
 eta_test = 1/(1+np.exp(-y_test_result))
 false = []
@@ -73,7 +71,7 @@ for l in range(1000):
     else:
         prob[l] = eta_test[0,l]
 confidence = prob[false]
-indx = argsort(confidence)[32:]
+indx = argsort(confidence)[28:]
 x_false20 = np.zeros((d,20)); y_real20 = np.zeros(20); y_pre20 = np.zeros(20)
 for l in range(20):
     x_false20[:,l] = x_false[:,int(indx[l])]
@@ -81,7 +79,7 @@ for l in range(20):
     y_pre20[l] = y_false_result[:,int(indx[l])]
 
 ## Plot the picture of 20 missclassified pictures
-fig = plt.figure(num='missclassified',figsize=(8,10))
+fig =  plt.figure(num='missclassified',figsize=(8,8))
 fig.suptitle("\"True\" represents real result, \"Pre\" represents predicted result\n")
 for l in range(20):
     plt.subplot(4,5,1+l)
@@ -94,3 +92,4 @@ for l in range(20):
     plt.title('True: %s, Pre: %s' %(str(true_title), str(pre_title)))
     plt.imshow(np.reshape(x_false20[:,l], (int(np.sqrt(d)), int(np.sqrt(d)))))
 plt.show()
+plt.close()
