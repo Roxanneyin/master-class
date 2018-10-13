b3908dd54615af3103b8810a76e35260e88b069b
Roxanneyin <roxanne.yin.1010@gmail.com>
2018-10-13 10:07:36 -0400

    added

--- EECS-545/homework 3/sto_sg_opt_smh.py
+++ EECS-545/homework 3/sto_sg_opt_smh.py
@@ -21,15 +21,15 @@ plt.figure()
 plt.scatter(x[0,gamma_rays], x[1,gamma_rays], s = 1, label = 'data of gamma rays')
 plt.scatter(x[0,neutrons], x[1,neutrons], s = 1, c = 'r', label = 'data of neutrons')
 
-# Initialization and plot learned line
+# Initialization and plot learned line with stochastic sub-gradient method
 x = np.mat(x); y = np.mat(y)
 add = 1; J = []
 x_tilda = np.insert(x, 0, values=add, axis=0)
 lamda = 0.001; omega = np.zeros(d); b = 0
-Iteration = 35
+Iteration = 5
 for j in range(Iteration):
-    u = 0; alpha = 100/(j+1); Loss = 0
-    for i in range(n):
+    ui = 0; alpha = 100/(j+1); N = np.random.permutation(n); Loss = 0
+    for i in N:
         h = 1 - y[0,i] * (np.dot(omega, x[:,i]) + b)
         if h[0,0] > 0:
             scale = 1
@@ -37,14 +37,14 @@ for j in range(Iteration):
             scale = 0
         else:
             scale = np.random.rand()
-        u += 1/n * (-y[0,i] * x_tilda[:,i]) * scale + lamda / n * np.mat([0, omega[0], omega[1]]).T
-        Loss += max(0, h[0,0])
-    J.append(Loss/n + lamda/2 * np.dot(omega, omega))
-    b -= alpha * u[0]; omega[0] -= alpha * u[1]; omega[1] -= alpha * u[2]
+        ui = 1/n * (-y[0,i] * x_tilda[:,i]) * scale + lamda / n * np.mat([0, omega[0], omega[1]]).T
+        b -= alpha * ui[0]; omega[0] -= alpha * ui[1]; omega[1] -= alpha * ui[2]
+        Loss = max(0, h[0, 0])
+        J.append(Loss/n + lamda/(2*n) * np.dot(omega, omega))
 
 print(b[0,0], omega[0], omega[1])
 t = pl.frange(0,8,0.01)
-n = pl.frange(1,Iteration)
+n = pl.frange(1,Iteration*n)
 plt.plot(t, -omega[0]/omega[1] * t - b[0,0]/omega[1], label = 'learned line')
 legend = plt.legend(loc='upper right', shadow=True)
 # Plot the objective function
--- EECS-545/homework 3/test.py
+++ EECS-545/homework 3/test.py
@@ -1,8 +1,5 @@
 import numpy as np
 
-a = np.mat([[1,2,3],[4,8,6]])
-b = 1
-c = np.insert(a, 0, values=b, axis=0)
-print(b,c)
-x = np.random.rand()
-print(x)
+n = 5
+N = np.random.permutation(n)
+print(N)
